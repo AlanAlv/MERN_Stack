355e822b699bc2542ce909dfd859c905d8020fe0
AlanAlv <alan.alv96@gmail.com>
2019-12-09 15:17:46 -0600

    Created react app

---
+++ frontend/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { BrowserRouter as Router, Route} from 'react-router-dom'
+import './App.css';
+
+import Navigation from './components/Navigation'
+import NotesList from './components/NotesList'
+import CreateNote from './components/CreateNote'
+import CreateUSer from './components/CreateUser';
+
+function App() {
+  return (
+    <Router>
+      <Navigation />
+        <Route exact path="/" component={NotesList} />
+        <Route exact path="/edit:id" component={CreateNote} />
+        <Route exact path="/create" component={CreateNote} />
+        <Route exact path="/user" component={CreateUSer} />
+
+    </Router>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/CreateUser.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class CreateUSer extends Component {
+    render() {
+        return (
+            <div>
+                Create User
+            </div>
+        )
+    }
+}
